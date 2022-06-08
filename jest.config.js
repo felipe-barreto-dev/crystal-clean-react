22258fb13ae58cddad344c49637bd24a922b2c0e
Marco Rhayden <Rhayden@arkansoftware.com>
2022-06-08 05:22:01 -0300

    chore: jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,14 @@
-export default {
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-    testEnvironment: "jsdom",
-    transformIgnorePatterns: [
-    "\\\\node_modules\\\\",
-    "\\.pnp\\.[^\\\\]+$"
-    ],
-    roots: ["<rootDir>/src"],
-    // Jest transformations -- this adds support for TypeScript
-    // using ts-jest
-    transform: {
-    "^.+\\.tsx?$": "ts-jest"
-    },
-    should contain `test` or `spec`.
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['\\\\node_modules\\\\', '\\.pnp\\.[^\\\\]+$'],
+  roots: ['<rootDir>/src'],
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
 };
