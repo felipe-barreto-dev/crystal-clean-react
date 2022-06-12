233288b9b28142b77b21ca27cc7e220b3b216da0
MarcoRhayden <Rhayden@arkansoftware.com>
2022-06-12 05:15:14 -0300

    refact: refact login to have context, states and form control

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './login-styles.scss';
-import { LoginHeader, Input, Footer } from '@/presentation/components';
+import { LoginHeader, Input, Footer, FormControl } from '@/presentation/components';
 import { MailIcon, VisibilityOffIcon } from '@/assets/svg/';
+import Context from '@/presentation/contexts/form/form-context';
 import colors from '../../styles/colors.scss'; // without alias to avoid jest errors
 
+type StateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
+
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  });
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <form className={Styles.form}>
-        <h2>Login</h2>
-        <Input
-          type="email"
-          name="email"
-          placeholder="E-mail"
-          icon={<MailIcon color={colors.background} />}
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Password"
-          icon={<VisibilityOffIcon color={colors.background} />}
-        />
-        <button className={Styles.submit} type="submit">
-          Login
-        </button>
-        <span className={Styles.link}>Create Account</span>
-      </form>
+      <Context.Provider value={state}>
+        <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            icon={<MailIcon color={colors.background} />}
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            icon={<VisibilityOffIcon color={colors.background} />}
+          />
+          <FormControl
+            buttonText={'Login'}
+            buttonStyle={Styles.submit}
+            spinnerStyle={Styles.customSpinner}
+          />
+          <span className={Styles.link}>Create Account</span>
+        </form>
+      </Context.Provider>
       <Footer />
     </div>
   );
