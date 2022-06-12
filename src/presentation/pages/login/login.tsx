be1d1704144ba61ef70bde5a7be07d17d39fabd7
MarcoRhayden <Rhayden@arkansoftware.com>
2022-06-12 19:33:05 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import colors from '../../styles/colors.scss'; // without alias to avoid jest errors
 import Styles from './login-styles.scss';
 import { LoginHeader, Input, Footer, FormControl } from '@/presentation/components';
 import { MailIcon, VisibilityOffIcon } from '@/assets/svg/';
 import Context from '@/presentation/contexts/form/form-context';
 import { Validation } from '@/presentation/protocols/validation';
-import colors from '../../styles/colors.scss'; // without alias to avoid jest errors
+import { Authentication } from '@/domain/usecases';
 
 type Props = {
   validation: Validation;
+  authentication: Authentication;
 };
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     errorMessage: '',
@@ -19,18 +21,37 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   });
 
   useEffect(() => {
-    if (validation) validation.validate('email', state.email);
-  }, [state.email]);
+    if (validation) {
+      setState({
+        ...state,
+        errorMessage: validation.validate('email', state.email)
+      });
+    }
+  }, [state.email, state.password]);
 
   useEffect(() => {
-    if (validation) validation.validate('password', state.password);
+    if (validation) {
+      setState({
+        ...state,
+        errorMessage: validation.validate('password', state.password)
+      });
+    }
   }, [state.password]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+    setState({
+      ...state,
+      isLoading: true
+    });
+    await authentication.auth({ email: state.email, password: state.password });
+  };
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form}>
+        <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input
             type="email"
