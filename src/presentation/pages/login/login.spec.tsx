7eb2f8660c3e362fddf4548e937b69e9c8c4e4cc
MarcoRhayden <Rhayden@arkansoftware.com>
2022-06-14 22:08:34 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -93,4 +93,11 @@ describe('Login Component', () => {
       password
     });
   });
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut();
+    simulateValidSubmit(sut);
+    simulateValidSubmit(sut);
+    expect(authenticationSpy.params).toBe(1);
+  });
 });
