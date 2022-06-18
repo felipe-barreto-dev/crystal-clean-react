ff9958ab838949b23511c789119184ea0768f337
MarcoRhayden <Rhayden@arkansoftware.com>
2022-06-18 02:35:53 -0300

    test: ensure LocalStorageAdapter calls localStorage with correct values

---
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+import 'jest-localstorage-mock';
+import { LocalStorageAdapter } from './local-storage-adapter';
+
+describe('LocalStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  test('Should call localStorage with correct values', () => {
+    const sut = new LocalStorageAdapter();
+    const key = faker.database.column();
+    const value = faker.random.word();
+    sut.set(key, value);
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
+  });
+});
