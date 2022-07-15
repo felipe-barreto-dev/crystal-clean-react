697e0dc6301b2547def7e4d7e082eedc28edf855
MarcoRhayden <Rhayden@arkansoftware.com>
2022-07-15 18:01:14 -0300

    feat: changes to LocalSaveAccessToken

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -4,7 +4,7 @@ import { SetStorage } from '@/data/protocols/cache/set-storage';
 export class LocalSaveAccessToken implements SaveAccessToken {
   constructor(private readonly setStorage: SetStorage) {}
 
-  async save(accessToken: string): Promise<void> {
-    this.setStorage.set('accessToken', accessToken);
+  async save(key: string, accessToken: string): Promise<void> {
+    this.setStorage.set(key, accessToken);
   }
 }
